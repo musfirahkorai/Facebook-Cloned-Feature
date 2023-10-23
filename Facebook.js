f20ec9a2d0b8a53b40380a89a4ad4bcfe01b8837
Musfira Murtaza <120357757+musfiramurtaza@users.noreply.github.com>
2023-10-23 10:41:52 +0500

    Create Facebook.js

---
+++ Facebook.js
@@ -0,0 +1,15 @@
+// Get references to the "Add Friend" and "Remove" buttons
+const addButton = document.getElementById("add");
+const removeButton = document.getElementById("removee");
+
+// Add an event listener to the "Add Friend" button
+addButton.addEventListener("click", () => {
+    // Add your logic here to handle adding a friend
+    alert("Friend added!");
+});
+
+// Add an event listener to the "Remove" button
+removeButton.addEventListener("click", () => {
+    // Add your logic here to handle removing a friend
+    alert("Friend removed!");
+});
